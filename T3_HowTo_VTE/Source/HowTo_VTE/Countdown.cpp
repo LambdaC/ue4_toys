e68b1ba5ce5af63f2e5373574c86e7caaaad2af0
LambdaC <studycwg@gmail.com>
2020-03-19 18:09:33 +0800

    add toy

---
+++ T3_HowTo_VTE/Source/HowTo_VTE/Countdown.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/TextRenderComponent.h"
+#include "TimerManager.h"
+#include "Countdown.h"
+
+// Sets default values
+ACountdown::ACountdown()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = false;
+
+    CountdownText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("CountdownNumber"));
+    CountdownText->SetHorizontalAlignment(EHTA_Center);
+    CountdownText->SetWorldSize(150.f);
+    RootComponent = CountdownText;
+
+    CountdownTime = 3; // seconds
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ACountdown::BeginPlay()
+{
+	Super::BeginPlay();
+
+    UpdateTimerDisplay();
+    GetWorldTimerManager().SetTimer(CountdownTimerHandle, this, &ACountdown::AdvanceTimer, 1.f, true);
+	
+}
+
+// Called every frame
+void ACountdown::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACountdown::UpdateTimerDisplay()
+{
+    CountdownText->SetText(FString::FromInt(FMath::Max(CountdownTime, 0)));
+}
+
+void ACountdown::AdvanceTimer()
+{
+    --CountdownTime;
+    UpdateTimerDisplay();
+
+    if (CountdownTime < 1)
+    {
+        // We're done counting down, so stop running the timer.
+        GetWorldTimerManager().ClearTimer(CountdownTimerHandle);
+        CountdownHasFinished();
+    }
+}
+
+void ACountdown::CountdownHasFinished_Implementation()
+{
+    // Change to a special readout
+    CountdownText->SetText(TEXT("GO!"));
+}
---
+++ T3_HowTo_VTE/readme.md
@@ -0,0 +1,3 @@
+This tutorial will show you how to expose variables and functions to the editor, use timers to delay or repeat code execution, and use events to communicate between Actors.
+
+https://docs.unrealengine.com/en-US/Programming/Tutorials/VariablesTimersEvents/index.html
