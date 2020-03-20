9a67d67261bb4541627f9182366b3486a74f9f0c
LambdaC <studycwg@gmail.com>
2020-03-20 14:47:57 +0800

    Set up a Project

---
+++ FPSProject/Source/FPSProject/FPSProjectGameModeBase.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+
+#include "FPSProjectGameModeBase.h"
+
+void AFPSProjectGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+
+    if (GEngine)
+    {
+        // Display a debug message for five seconds. 
+        // The -1 "Key" value (first argument) indicates that we will never need to update or refresh this message.
+        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameMode!"));
+    }
+    UE_LOG(LogTemp, Warning, TEXT("Hello, World!"));
+}
---
+++ FPSProject/readme.md
@@ -0,0 +1,14 @@
+## Goals
+
+The purpose of this tutorial is show you how to build a basic First Person Shooter (FPS) using C++.
+
+## Objectives
+
+By the end of this tutorial you'll be able to:
+
+- Set up a Project
+- Implement a Character
+- Implement a Projectile
+- Animate a Character
+
+https://docs.unrealengine.com/en-US/Programming/Tutorials/FirstPersonShooter/index.html
