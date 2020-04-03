1ac80da2fbe0f4e54886981e4fea9bc6ae24524a
LambdaC <studycwg@gmail.com>
2020-04-03 19:09:48 +0800

    add Pong

---
+++ T8_Pong/Source/Pong/PongGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PongGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PONG_API APongGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
